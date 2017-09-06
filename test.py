dc111a8203693299fe8f3f39eb17ab4cd22af3e5
Kaiming Cheng <kaimingcheng@KaimingdeMacBook-Air.local>
2017-09-06 15:09:49 +0800

    test and img

---
+++ test.py
@@ -0,0 +1,18 @@
+from mysort import insert_sort
+from mysort import shell_sort
+from mysort import select_sort
+from mysort import heap_sort
+from mysort import bubble_sort
+from mysort import quick_sort_use_space
+from mysort import quick_sort
+from mysort import radix_sort
+l = [994,246,37,668,254,480,739,770,256]
+
+#print(insert_sort(l))
+#print(shell_sort(l))
+#print(select_sort(l))
+#print(heap_sort(l))
+#print(bubble_sort(l))
+#print(quick_sort_use_space(l))
+#print(quick_sort(l,0,len(l)-1))
+print(radix_sort(l))
